aa0aeb815d21262f42bb5a4e5f6eba6a151752c6
sa34263420 <sa34263420@gmail.com>
2021-04-01 11:33:16 +0800

    function to arrow function

--- 0401/function.js
+++ 0401/function.js
@@ -1,26 +1,15 @@
 import React from 'react';
 
-function App() {
-      return (
-         <div>
-            <Header/>
-            <Content/>
-         </div>
-      );
-}
-function Header(){
-      return (
-         <div>
-            <h1>Header</h1>
-         </div>
-      );
-}
-function Content(){
-      return (
-         <div>
-            <h2>Content</h2>
-            <p>The content text!!!</p>
-         </div>
-      );
-}
+
+const App = () => (<div>
+   <Header/>
+   <Content/>
+</div>)
+const Header = () => (<div>
+   <h1>Header</h1>
+</div>)
+const Content = () => (<div>
+   <h2>Content</h2>
+   <p>The content text!!!</p>
+</div>)
 export default App;
