950dd384dda93f5fbb6a6bafc324035dc96f8b51
sa34263420 <sa34263420@gmail.com>
2021-04-08 09:56:49 +0800

    0401_homework

---
+++ 0401/App3.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const App = () => (<div>
+   <Header/>
+   <Content/>
+</div>)
+const Header = () => (<div>
+   <h1>Header</h1>
+</div>)
+const Content = () => (<div>
+   <h2>Content</h2>
+   <p>The content text!!!</p>
+</div>)
+export default App;
