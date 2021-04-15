34b20263e2ead1736d24a7622a6496cf841fddd9
sa34263420 <sa34263420@gmail.com>
2021-04-15 11:38:42 +0800

    0415第三節練習(set state)

--- 0415/0415_app/src/App.js
+++ 0415/0415_app/src/App.js
@@ -7,6 +7,19 @@ class Clock extends React.Component{
     super(props);
     this.state = {date: new Date() };
   }
+  componentDidMount() {
+    this.timerID = setInterval(()=> this.tick(),1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+  
   render(){
     return (
       <div>
