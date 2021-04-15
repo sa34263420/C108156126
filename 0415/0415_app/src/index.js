7e02b192029797a7f1ee2ad636900ecc5c916433
sa34263420 <sa34263420@gmail.com>
2021-04-15 11:16:01 +0800

    0415第三節練習(重新設定初始)

--- 0415/0415_app/src/index.js
+++ 0415/0415_app/src/index.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Clock(props) {
-  return (
+function tick() {
+  const element = (
     <div>
       <h1>Hello, world!</h1>
-      <h2>It is {props.date.toLocaleTimeString()}.</h2>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
     </div>
   );
-}
-
-function tick() {
   ReactDOM.render(
-    <Clock date={new Date()} />,
+    element,
     document.getElementById('root')
   );
 }
+
 setInterval(tick, 1000);
 
 // If you want to start measuring performance in your app, pass a function
