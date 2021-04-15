03d82665d194609d2307aa3ce5b87526a5d49c38
sa34263420 <sa34263420@gmail.com>
2021-04-15 10:51:45 +0800

    功能寫入

--- 0415/0415_app/src/App.js
+++ 0415/0415_app/src/App.js
@@ -13,6 +13,9 @@ class Car extends React.Component {
       year: 1964
     };
   }
+  changeColor = () => {
+    this.setState({color: "blue"});
+  }
   render() {
     return <div>
     <h1>My {this.state.brand}</h1>
@@ -23,6 +26,7 @@ class Car extends React.Component {
     </p>
     <button
       type="button"
+      onClick={this.changeColor}
     >Change color</button>
   </div>
   }
