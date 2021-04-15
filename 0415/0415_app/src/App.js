c556f4b36599154c5fa60d5e6c58a7a1978f7c98
sa34263420 <sa34263420@gmail.com>
2021-04-15 10:48:38 +0800

    修改constructor

--- 0415/0415_app/src/App.js
+++ 0415/0415_app/src/App.js
@@ -4,9 +4,14 @@ import React from 'react';
 
 
 class Car extends React.Component {
-  constructor() {
-    super();
-    this.state = {color: "red"};
+  constructor(props) {
+    super(props);
+    this.state = {
+      brand: "Ford",
+      model: "Mustang",
+      color: "red",
+      year: 1964
+    };
   }
   render() {
     return <h2>I am a {this.state.color} Car!</h2>;
