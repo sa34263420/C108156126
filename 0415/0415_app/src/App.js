1ec18235645f406fb06ce8765c337c53286c8e5a
sa34263420 <sa34263420@gmail.com>
2021-04-15 11:29:41 +0800

    0415第三節練習(改成class compoment)

--- 0415/0415_app/src/App.js
+++ 0415/0415_app/src/App.js
@@ -2,13 +2,19 @@
 import './App.css';
 import React from 'react';
 
-function Clock(props){
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {props.date.toLocaleTimeString()}.</h2>
-    </div>
-  );
+class Clock extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date() };
+  }
+  render(){
+    return (
+      <div>
+        <h1>This is cless compoment!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
 export default Clock;
