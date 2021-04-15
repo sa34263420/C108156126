bd133aee0a7da69d5ee88236bfbaae3fa3bf59f8
sa34263420 <sa34263420@gmail.com>
2021-04-15 11:26:11 +0800

    0415第三節練習(改成render格式)

--- 0415/0415_app/src/App.js
+++ 0415/0415_app/src/App.js
@@ -2,36 +2,14 @@
 import './App.css';
 import React from 'react';
 
-
-class Car extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      brand: "Ford",
-      model: "Mustang",
-      color: "red",
-      year: 1964
-    };
-  }
-  changeColor = () => {
-    this.setState({color: "blue"});
-  }
-  render() {
-    return <div>
-    <h1>My {this.state.brand}</h1>
-    <p>
-      It is a {this.state.color}
-      {this.state.model}
-      from {this.state.year}.
-    </p>
-    <button
-      type="button"
-      onClick={this.changeColor}
-    >Change color</button>
-  </div>
-  }
+function Clock(props){
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {props.date.toLocaleTimeString()}.</h2>
+    </div>
+  );
 }
 
+export default Clock;
 
-
-export default Car;
