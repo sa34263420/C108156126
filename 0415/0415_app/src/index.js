fb1f40b9f42698f1144c91bffecc11de2f047642
sa34263420 <sa34263420@gmail.com>
2021-04-15 11:06:42 +0800

    0415第三節練習(初始)

--- 0415/0415_app/src/index.js
+++ 0415/0415_app/src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './index.css';
-import Car from './App';
-//import reportWebVitals from './reportWebVitals';
 
+function Clock(props) {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {props.date.toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
 
-  ReactDOM.render(<Car />, document.getElementById('root'));
+function tick() {
+  ReactDOM.render(
+    <Clock date={new Date()} />,
+    document.getElementById('root')
+  );
+}
+setInterval(tick, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
