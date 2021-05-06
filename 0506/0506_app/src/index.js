692ffd8ad3b7083bb2cbc3098ce117e7361df904
sa34263420 <sa34263420@gmail.com>
2021-05-06 11:12:49 +0800

    加入App1

--- 0506/0506_app/src/index.js
+++ 0506/0506_app/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
+import App1 from './App1.js';
 ReactDOM.render(<App/>, document.getElementById('root'));
+//ReactDOM.render(<App1/>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
