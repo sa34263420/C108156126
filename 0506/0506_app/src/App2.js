667a422587cf8d90d30f4f7ef5f9b4923be3d7c5
sa34263420 <sa34263420@gmail.com>
2021-05-06 11:41:13 +0800

    新增處理submit&訊息

--- 0506/0506_app/src/App2.js
+++ 0506/0506_app/src/App2.js
@@ -1,26 +1,40 @@
 import React from "react";
 class App2 extends React.Component {
- constructor(props) {
-   super(props);
-   this.state = { username: '' };
- }
- myChangeHandler = (event) => {
-   this.setState({username: event.target.value});
- }
- render() {
-    let header = '';
-    if(this.state.username){
-        header=<h1>Hello {this.state.username}</h1>
-    }else{
-        header='';
-    }
-   return (
-     <form>
-     {header}
-     <p>Enter your name:</p>
-     <input type='text' onChange={this.myChangeHandler} />
-     </form>
-   );
- }
-}
+    constructor(props) {
+        super(props);
+        this.state = { username: '',
+        datasubmitted: false};
+      }
+      myChangeHandler = (event) => {
+        this.setState({username: event.target.value});
+      }
+      mySubmitHandler = (event) => {
+        event.preventDefault();
+        this.setState({datasubmitted: true});
+        console.log(this.state.username + ' 資料送出');
+      }
+      render() {
+        let header = '';
+        if (this.state.username) {
+          header = <h1>Hello {this.state.username}</h1>;
+        } else {
+          header = '';
+        }
+        let message = '';
+        if (this.state.datasubmitted) {
+          message = <h2>{this.state.username} data submitted</h2>;
+        } else {
+          message = '';
+        }
+        return (
+          <form onSubmit={this.mySubmitHandler}>
+          {header}
+          {message}
+          <p>Enter your name:</p>
+          <input type='text' onChange={this.myChangeHandler} />
+          <input type='submit' value='Submit' />
+          </form>
+        );
+      }
+      }
 export default App2;
